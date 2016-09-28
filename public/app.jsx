ef147c03f95a6e05e6a665c33046ed80fc208da9
Mike Murray <michaelbrazell@gmail.com>
2016-09-27 23:58:36 -0400

    divide into components

--- public/app.jsx
+++ public/app.jsx
@@ -1,26 +1,35 @@
 var GreeterMessage = React.createClass({
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
         <div>
-          <h1>Some H1</h1>
-          <p>Some paragraph</p>
+          <h1>Hello {name}!</h1>
+          <p>{message}</p>
         </div>
       );
     }
 });
 
 var GreeterForm = React.createClass({
+  onFormSubmit: function (e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
   render: function() {
     return (
-      <form>
+      <form onSubmit={this.onFormSubmit}>
         <input type="text" ref="name" />
         <button>Set Name</button>
       </form>
     );
   }
 });
-
-
 
 var Greeter = React.createClass({ // This is a react component. createClass() is the most common react method.
   getDefaultProps: function () { // Sets a default prop for the name prop but can be overwritten
@@ -34,44 +43,26 @@ var Greeter = React.createClass({ // This is a react component. createClass() is
       name: this.props.name
     };
   },
-  onButtonClick: function (e) { // This function gets passed an event handler in (e)
-    e.preventDefault();
-    var nameRef = this.refs.name;
-    var name = nameRef.value; // Refs refers to the React attribute on the input.
-    nameRef.value = ''; // Removes the name from the input after submitting
-
-    if (typeof name === 'string' && name.length > 0) { // CHecks if name is a string and is longer than 0, if so...
-      this.setState({ // Updates the state and re-renders the component.
-        name: name
-      });
-    }
+  handleNewName: function (name) { // This function gets passed an event handler in (e)
+    this.setState({
+      name: name
+    })
   },
   render: function () { // Only thing that's required is render method; needs to return JSX
     var name = this.state.name; // Refers to the state from onButtonClick method
     var message = this.props.message;
     return (
       <div>
-        <h1>Hello {name}</h1>
-        <p>{message + '!!!!'}</p>
-
-        <GreeterMessage/>
-
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name" />
-          <button>Set Name</button>
-        </form>
-
-        <GreeterForm/>
-
+        <GreeterMessage name={name} message={message}/>
+        <GreeterForm onNewName={this.handleNewName}/>
       </div>
     );
   }
 });
 
 var firstName = 'Mike';
-var customMessage = 'This is a custom message!'
 
 ReactDOM.render(
-  <Greeter name={firstName} message={customMessage} />, // This gets passed from the method above
+  <Greeter name={firstName}/>, // This gets passed from the method above
   document.getElementById('app')
 );
